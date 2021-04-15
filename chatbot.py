6dae0e748c1dabf2bfc28cb122c5d8c0e3801627
progwiz-ishaan <76903228+progwiz-ishaan@users.noreply.github.com>
2021-04-15 12:38:21 +0530

    o

---
+++ chatbot.py
@@ -0,0 +1,14 @@
+# This is a chatbot.
+print("Hi! I am ChatBot 142. I am a chatbot\nI like animals and I \
+    love to talk about food.")# Intorduction
+
+name = input("What is your name?: ")
+print("Hello", name, "nice to meet you!")
+
+date = input("I am not very good with dates. \
+    What's the year?: ")
+print("Yes, I think that is correct. Thanks!")
+my_age = input("Can you guess my age?: ")
+print("How did you guess the correct age?")
+print("I will be 100 in", 100 - my_age, 'years.\
+     That will be in', int(date) + (100 - my_age), '.')
